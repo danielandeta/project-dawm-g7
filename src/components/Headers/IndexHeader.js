f142df6c4fbe7112b0e1a3f80fb1ae1229edc44a
anamabri <ana_mary53@hotmail.com>
2021-06-26 01:41:54 -0500

    imagenes

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -29,7 +29,7 @@ function IndexHeader() {
           className="page-header-image"
           style={{
             backgroundImage:
-              "url(" + require("assets/img/header.jpg").default + ")",
+              "url(" + require("assets/img/fondo.jpg").default + ")",
           }}
           ref={pageHeader}
         ></div>
@@ -43,28 +43,6 @@ function IndexHeader() {
             <h1 className="h1-seo">Now UI Kit.</h1>
             <h3>A beautiful Bootstrap 4 UI kit. Yours free.</h3>
           </div>
-          <h6 className="category category-absolute">
-            Designed by{" "}
-            <a href="http://invisionapp.com/?ref=creativetim" target="_blank">
-              <img
-                alt="..."
-                className="invision-logo"
-                src={require("assets/img/invision-white-slim.png").default}
-              ></img>
-            </a>
-            . Coded by{" "}
-            <a
-              href="https://www.creative-tim.com?ref=nukr-index-header"
-              target="_blank"
-            >
-              <img
-                alt="..."
-                className="creative-tim-logo"
-                src={require("assets/img/creative-tim-white-slim2.png").default}
-              ></img>
-            </a>
-            .
-          </h6>
         </Container>
       </div>
     </>
