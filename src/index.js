e8003fade81235d88d4b381fe0f65550e8cd1594
Daniela <dlandeta@espol.edu.ec>
2021-06-26 17:52:24 -0500

    cards, faltan foto y navbar

--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
-/*
 
-=========================================================
-* Now UI Kit React - v1.5.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/now-ui-kit-react
-* Copyright 2021 Creative Tim (http://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/now-ui-kit-react/blob/main/LICENSE.md)
-
-* Designed by www.invisionapp.com Coded by www.creative-tim.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-
 // styles for this kit
 import "assets/css/bootstrap.min.css";
 import "assets/scss/now-ui-kit.scss?v=1.5.0";
