6706ede325d3d8840a40edfb3013aeadffebeb5b
Daniela <dlandeta@espol.edu.ec>
2021-07-14 01:26:39 -0500

    falta centrar form

--- src/views/index-sections/Formulario.js
+++ src/views/index-sections/Formulario.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
 import IndexNavbar from "../../components/Navbars/IndexNavbar.js";
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
-import Catalogo from "views/index-sections/Catalogo.js";
+//import Grid from '@material-ui/core/Grid';
 import {
   Button,
   Input,
@@ -37,6 +37,32 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Formulario() {
   const classes = useStyles();
+  const [datos, setDatos]=useState({
+    nombre: '',
+    apellidos: '',
+    celular: '',
+    email: '',
+    descripcion: ''
+  })
+
+  const handleChange = (e) => {
+    setDatos({
+      ...datos,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleChecked = (e)=>{
+    setDatos({
+      ...datos,
+      [e.target.nombre]:e.target.checked,
+    })
+  };
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    alert("El formulario ha sido enviado");
+  };
 
   return( 
     <>
@@ -45,15 +71,65 @@ export default function Formulario() {
           <Container>
             <Row>
               <Col className="ml-auto mr-auto text-center" md="8">
-                <h2 className="title">Encuentranos</h2>
+                <h2 className="title">Formulario de contacto</h2>
                 <h5 className="description">
-                  Aquí podrás encontrar la dirección donde estamos ubicados
-                  y te mostramos las maneras de ponerte en contacto con
-                  nosotros para realizarnos consultas y realizar alguna compra.
+                  Aquí deberás llenar todos los campos para podernos contactar contigo y darte información más detallada sobre 
+                  nuestras deliciosas mermeladas artesanales.
                   ¡Gracias por elegirnos!
                 </h5>
               </Col>
             </Row>
+
+              <form className="ml-auto mr-auto col-center" onSubmit={handleSubmit}>
+                <div className="col-md-3 col-center">
+                  <label htmlFor="nombre">Nombre: </label>
+                    <input 
+                      type="text" 
+                      name="nombre" 
+                      className="form-control my-2"
+                      value={datos.nombre}
+                      onChange={handleChange}
+                    />
+                </div>
+                <div className="col-md-3">
+                  <label htmlFor="apellidos">Apellidos: </label>
+                    <input 
+                      type="text" 
+                      name="apellidos" 
+                      className="form-control my-2"
+                      value={datos.apellidos}
+                      onChange={handleChange}
+                    />
+                </div>
+                <div className="col-md-3">
+                  <label htmlFor="email">Email: </label>
+                    <input 
+                      type="text" 
+                      name="email" 
+                      className="form-control my-2"
+                      value={datos.email}
+                      onChange={handleChange}
+                    />
+                </div>
+                <div className="col-md-3">
+                  <label htmlFor="celular">Celular: </label>
+                    <input 
+                      type="text" 
+                      name="celular" 
+                      className="form-control my-2"
+                      value={datos.celular}
+                      onChange={handleChange}
+                    />
+                </div>
+                <div className="col-md-3">
+                  <label>
+                    ¿Qué información le gustaría conocer?:
+                    <textarea className="form-control my-2" value={datos.descripcion} onChange={handleChange} />
+                  </label>
+                </div>
+                <button type="submit" className="btn btn-primary">Enviar</button>
+              </form>
+
           </Container>
         </div>
     </>
