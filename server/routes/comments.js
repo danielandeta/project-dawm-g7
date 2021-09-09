8f150613e08149cb1f75343bbafa774a2b907eac
William Venegas <rvenegas@espol.edu.ec>
2021-09-09 11:59:56 -0500

    Permisos

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const sequelize = require('../models/index.js').sequelize;
+var initModels = require("../models/init-models");
+var models = initModels(sequelize); 
 const router = express.Router()
 const { Comments } = require('../models')
 const { validateToken } = require('../middlewares/AuthMiddleware')
@@ -20,5 +23,22 @@ router.post('/', validateToken, async(req, res) => {
   comment.username = username
   await Comments.create(comment)
   res.json(comment)
+})
+
+router.post('/verificar', validateToken, async(req, res) => {
+  const username = req.user.username
+  const user = await models.users.find({
+    where: {
+      username : username
+    }
+  })
+  if (user.rol == "ADMIN"){
+    res.json({admin : user.rol})
+  } else if (user.rol == "CLIENT") {
+    res.json({client: user.rol})
+  } else {
+    res.json({err: "Usuario no esta logeado"})
+  }
+  console.log(username)
 })
 module.exports = router 
